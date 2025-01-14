625285a463610f1825c9947e4289772e7a19c00f
MarcelArt <badman.islemon@gmail.com>
2025-01-15 00:51:38 +0700

    Add metric monitoring

--- routes/route.go
+++ routes/route.go
@@ -10,6 +10,7 @@ import (
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/limiter"
 	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/fiber/v2/middleware/monitor"
 	"github.com/gofiber/swagger"
 )
 
@@ -26,6 +27,8 @@ func SetupRoutes(app *fiber.App) {
 		URL:         "http://example.com/doc.json",
 		DeepLinking: false,
 	}))
+
+	app.Get("/metrics", monitor.New())
 
 	authMiddleware := middlewares.NewAuthMiddleware(repositories.NewUserRepo(database.GetDB()))
 
