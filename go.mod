575df98320a32ef1ed8793c16693329351d4d83c
MarcelArt <badman.islemon@gmail.com>
2025-01-15 00:47:31 +0700

    Add rate limiter

--- go.mod
+++ go.mod
@@ -29,9 +29,11 @@ require (
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mattn/go-runewidth v0.0.16 // indirect
 	github.com/morkid/gocache v1.0.0 // indirect
+	github.com/philhofer/fwd v1.1.3-0.20240916144458-20a13a1f6b7c // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
 	github.com/rogpeppe/go-internal v1.13.1 // indirect
 	github.com/swaggo/files/v2 v2.0.1 // indirect
+	github.com/tinylib/msgp v1.2.5 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.58.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
--- routes/route.go
+++ routes/route.go
@@ -1,11 +1,14 @@
 package routes
 
 import (
+	"time"
+
 	"github.com/MarcelArt/app_standard/database"
 	"github.com/MarcelArt/app_standard/middlewares"
 	"github.com/MarcelArt/app_standard/repositories"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/limiter"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/gofiber/swagger"
 )
@@ -13,6 +16,10 @@ import (
 func SetupRoutes(app *fiber.App) {
 	app.Use(cors.New())
 	app.Use(logger.New())
+	app.Use(limiter.New(limiter.Config{
+		Max:        20,
+		Expiration: 30 * time.Second,
+	}))
 
 	app.Get("/swagger/*", swagger.HandlerDefault)     // default
 	app.Get("/swagger/*", swagger.New(swagger.Config{ // custom
